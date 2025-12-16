052ab5e671a4e4108116a2391904e7148b1759e0
AADITHYA <aadithyasreedevi@gmail.com>
2025-12-16 18:45:43 +0530

    Efficiency tweaks by using facedistance metric

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import shutil
 import os
 import cv2
 import face_recognition
+import numpy as np
 from pathlib import Path
+from engine import FaceEngine
 BASE_DIR = Path(__file__).resolve().parent
 # img=face_recognition.load_image_file(BASE_DIR/"testimage"/"img1.webp")
 # face_location1=face_recognition.face_locations(img)
@@ -26,25 +28,34 @@ BASE_DIR = Path(__file__).resolve().parent
 #     result=face_recognition.compare_faces([fingerprint[0]],)
 infolder="testimage"
 outfolder="sorted"
+engine=FaceEngine(use_cnn=True)
 known_fingerprints=[]
 known_ids=[]
 nextid=1
 for filenames in os.listdir(infolder):
     fullpath=os.path.join(infolder,filenames)
     print(f"Scanning:{filenames}...")
-    img=face_recognition.load_image_file(fullpath)
-    loc=face_recognition.face_locations(img)
+    img,loc=engine.get_faces_robust(fullpath)
+    if len(locs)==0:
+        print(f" ->No faces found moving to nofaces folder.")
+        nofacefold=os.path.join(outfolder,"NoFaces")
+        os.makedirs(nofacefold,exist_ok=True)
+        shutil.copy2(fullpath,nofacefold)
+        continue
     fingerprint=face_recognition.face_encodings(img,loc)
     for face_encoding in fingerprint:
-        matches=[]
         personid=None
         if len(known_fingerprints)>0:
-            matches=face_recognition.compare_faces(known_fingerprints,face_encoding)
-        if True in matches:
-            fp_index=matches.index(True)
-            personid=known_ids[fp_index]
-            print(f"found person -> {personid}")
+            face_distances=face_recognition.face_distance(known_fingerprints,face_encoding)
+            best_match_index=np.argmin(face_distances)
+            if face_distances[best_match_index]<0.6:
+                personid=known_ids[best_match_index]
+                print(f"found person -> {personid}(distance:{face_distances[best_match_index]})")
+            else:
+                personid=None
         else:
+            personid=None
+        if personid is None:
             personid=nextid
             print(f"->New face...ID: {personid}")
             known_fingerprints.append(face_encoding)
